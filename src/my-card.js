fc2612bc7235e54b87135029ebf1918c3ff9ce81
Tylar Sanderson <tas6466@psu.edu>
2024-09-23 16:07:40 -0400

    polaris

--- src/my-card.js
+++ src/my-card.js
@@ -22,6 +22,49 @@ export class MyCard extends LitElement {
       :host {
         display: block;
       }
+      .wrapper {
+        display: flex;
+      }
+      .card  {
+        width: 250px;
+        margin: 8px;
+        background-color: #203731;
+        border-style: solid;
+        border-width: 4px;
+        border-radius: 16px;
+        border-color: #FFB612;
+        padding: 16px;
+        margin-bottom: 8px;
+        font-family: stencil;
+        text-align: center;
+        color: floralwhite;
+      }
+      .logo {
+        width: 250px;
+        border-radius: 8px;
+        border-style: solid;
+        border-color: #FFB612;
+        border-width: 2px;
+      }
+      button {
+        font-family: stencil;
+        font-size: 12pt;
+        color: #203731;
+        height: 30px;
+        margin: 8px;
+        border-style: ridge;
+        border-color: #203731;
+        background-color: #FFB612;
+        border-radius: 8px;
+        cursor: pointer;
+      }
+      button:hover {
+        background: white;
+        color: #203731;
+      }
+      p {
+        font-family: stencil;
+      }
     `;
   }
 
@@ -31,7 +74,7 @@ export class MyCard extends LitElement {
 
   static get properties() {
     return {
-      title: { type: String },
+      title: { type: String }, 
     };
   }
 }
